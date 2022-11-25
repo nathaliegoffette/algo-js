8e40f1bf76b931082bf2509fda771d2ff93e0724
Nathalie Goffette <nathaliegoffette@gmail.com>
2022-11-25 10:57:20 +0100

    3.0.js

---
+++ 3.0.js
@@ -0,0 +1,5 @@
+let arr = ["Arno Volts" ,"Aurélien Mariaule" ,"Aurore Lemaire" ,"Benjamin Porsont" ,"Céline Verreydt" ,"Corentin Miserque" ,"Dominique Coppée" ,"Edouard de Romrée de Vichenet" ,"Hugo Goorickx ","Jofrey Houyoux" ,"Jonathan Manes" ,"jonathan Bajoux" ,"Laura Wilhelmi" ,"Lysie Soyez" ,"Marnie Benalia" ,"Mathilde Cornelis" ,"Milo Bonnet" ,"Nadim El Nakadi" ,"Nathalie Vanden Abeele" ,"Nathalie Goffette" ,"Nour Everaert" ,"Pierre Mauriello" ,"Quentin Bource ","Virginie Dourson"];
+
+for (let elem of arr) {
+    console.log(elem);
+  }
