ebe608fb88bf27c6bed45dfe82784773a3172e3d
Nathalie Goffette <nathaliegoffette@gmail.com>
2022-11-25 12:32:05 +0100

    3.3.js

---
+++ 3.3.js
@@ -0,0 +1,18 @@
+let arr1 = ["1", "2", "3", "4", "5"];
+let arr2 = [];
+
+
+arr2.push(...arr1);
+
+console.log(arr2)
+
+
+
+
+/*
+let arr1 = ["1", "2", "3", "4", "5"];
+let arr2 = arr1;
+
+
+console.log(arr2.slice());
+*/
