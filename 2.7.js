cb3a886f69bcc60b286b966c2f13ea1d55afa9dd
Nathalie Goffette <nathaliegoffette@gmail.com>
2022-11-25 10:46:00 +0100

    2.7.js

---
+++ 2.7.js
@@ -0,0 +1,13 @@
+let n = Number(prompt("Give me a number"));
+let sum = 0;
+
+
+for (let i=0; i<n; i++){
+    
+    let x = Number(prompt("give me another number"));
+
+    sum+=x;
+}
+
+alert(sum);
+
