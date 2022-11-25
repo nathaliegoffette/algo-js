3035859f4b216ad0ead6aeeaafb33c1dae6e4011
Nathalie Goffette <nathaliegoffette@gmail.com>
2022-11-25 13:55:33 +0100

    3.4.js

---
+++ 3.4.js
@@ -0,0 +1,7 @@
+let numbers = [1, 17, 6, 0, 68, 32];
+
+let smallest_number = Math.min(...numbers);
+let largest_number = Math.max(...numbers);
+
+console.log("Smallest Value:", smallest_number); 
+console.log("Largest Value:", largest_number); 
