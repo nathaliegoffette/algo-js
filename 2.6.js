63d9c59bc40290eee1014304c1268cb62f98b157
Nathalie Goffette <nathaliegoffette@gmail.com>
2022-11-25 09:14:35 +0100

    resolution 2.6.js

--- 2.6.js
+++ 2.6.js
@@ -1,23 +1,26 @@
 let day = prompt("Give me a number from 1 to 7");
 
-if (day="1"){
+if (day=="1"){
     console.log("monday");
 }
-else if (day="2"){
+else if (day=="2"){
     console.log("tuesday");
 }
-else if (day="3"){
+else if (day=="3"){
     console.log("wednesday");
 }
-else if (day="4"){ 
+else if (day=="4"){ 
     console.log("thursday");
 }
-else if (day="5"){
+else if (day=="5"){
     console.log("friday");
 }
-else if (day="6"){
+else if (day=="6"){
     console.log("saturday");
 }
-else (day="7"){
+else if (day=="7"){
     console.log("sunday");
+}
+else {
+    console.log("Read the question again !");
 }
